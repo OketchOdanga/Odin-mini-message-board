ab977ed1dd8a9310cddbd6f792e6a2ec8ca9e54d
Austine Odanga <austinoketches@gmail.com>
2024-08-29 11:51:50 +0300

    feat: updated routes

---
+++ routes/router.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const router = express.Router();
+
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+//index route
+router.get('/', (req, res) => 
+  res.render("index", { messages: messages})
+);
+
+// new message form route
+router.get('/new', (req ,res) => 
+  res.render("form")
+);
+
+// handle new message
+router.post('/new', (res, req) => {
+  console.log(req.body);
+  const { messageText, messageUser } = req.body;
+  messages.push({text: messageText, user: messageUser, added: new Date() });
+  console.log(messages);
+  console.log(messages.length);
+  res.redirect('/');
+});
+
+//
+router.get('/message/:id', (req, res) => {
+  const message = messages[req.params.id];
+  res.render('message', {title: "Message Detais", message: message});
+});
+
+module.exports = router;
