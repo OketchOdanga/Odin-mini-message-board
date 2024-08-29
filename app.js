79e0687eedde7c79ee087ceeaad0be3c37885d0f
Austine Odanga <austinoketches@gmail.com>
2024-08-29 11:53:08 +0300

    feat: refactored server

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const app = express();
+const path = require('node:path');
 
-//
-app.get('/', (req, res) => {
-    res.send("Hello world")
-})
-//
+const indexRouter = require('./routes/router');
 
 //add middlewares.
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs")
+
+//used to parse the form data into req.body
+app.use(express.urlencoded({extended: true}));
+
 //add router middlewares
-//add routes
+app.use('/', indexRouter)
+
 //add error handlers
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send(err.message);
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`listening on port ${PORT}!`));
